8333e0a56f4ac8916a1b54726ad164b97c4ab1cf
luizgustavoou <luizgustavooumbelino@gmail.com>
2024-03-01 18:33:31 -0300

    adicionar scroll

---
+++ chat-ui/src/hooks/useWebsocket.ts
@@ -0,0 +1,50 @@
+// import { useEffect, useRef } from "react";
+// import { Socket, io } from "socket.io-client";
+
+// export const useWebsocket = (url: string) => {
+//   const ws = useRef<Socket | null>(null);
+
+//   useEffect(() => {
+//     const socket = io(url);
+
+//     ws.current = socket;
+
+//     socket.on("connect", () => console.log("Conectado ao Websocket"));
+//     socket.on("disconnect", () => console.log("Desconectado ao Websocket"));
+
+//     return () => {
+//       close();
+//     };
+//   }, []);
+
+//   const addEventListener = async (
+//     event: string,
+//     callback: (args: any) => void
+//   ) => {
+//     // console.log("adicionado event listener!");
+
+//     ws.current?.on(event, callback);
+//   };
+
+//   const removeEventListener = async (
+//     event: string,
+//     callback: (args: any) => void
+//   ) => {
+//     // console.log("removido event listener!");
+//     ws.current?.off(event, callback);
+//   };
+
+//   const emit = (event: string, data: unknown) => {
+//     ws.current?.emit(event, data);
+//   };
+
+//   const connect = () => {
+//     ws.current?.connect();
+//   };
+
+//   const close = () => {
+//     ws.current?.disconnect();
+//   };
+
+//   return { ws, addEventListener, removeEventListener, emit };
+// };
